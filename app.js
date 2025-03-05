1aa3adc9bef9d46705eaaaa3e59ec3540d0c9eb8
EdsonCasta <dedsoncasta07@gmail.com>
2025-03-05 13:26:36 -0500

    dia 2

--- app.js
+++ app.js
@@ -48,3 +48,30 @@ if (numeroDiez == stringDiez) {
   console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
 
 }
+
+// Desafio dia 02
+
+let nombre = prompt("¿Cuál es tu nombre?");
+
+let edad = prompt("¿Cuántos años tienes?");
+
+let lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?");
+
+console.log(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!`);
+
+// EJERCICIO OPCIONAL
+
+function respuesta() {
+  let pregunta = prompt(`¿Te gusta estudiar ${lenguaje}? Responde con el número 1 para SÍ o 2 para NO.`);
+  
+  if (pregunta == 1) {
+    console.log("1 > ¡Muy bien! Sigue estudiando y tendrás mucho éxito.")
+  } else if (pregunta == 2) {
+    console.log("2 > Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?")
+  } else {
+    alert("Ingrese solo 1 o 2 dependiendo su respuesta.");
+    respuesta();
+  }
+};
+
+respuesta();
